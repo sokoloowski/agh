f44eebbf2362df28b6c5413b19fdcbeaf5c0d597
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2020-12-08 16:12:58 +0100

    Nowe zajęcia

---
+++ pp1/2020-12-08/listy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void dopisz(){}
+void wypisz(){}
+void wstaw(){}
+void usun(){}
+void szukaj(){} // zwraca wskaźnik
+void kopiuj(){}
+void dodaj(){}
+
+int main(void)
+{
+    return 0;
+}
