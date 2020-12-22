b24ae6b8141124d6231c886c44e1fc65340aeaef
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2020-12-22 17:04:53 +0100

    Rename

--- pp1/2020-12-22/serwer1.c
+++ pp1/2020-12-22/serwer1.c
@@ -4,8 +4,13 @@
 
 int main(void)
 {
-    int status, gniazdo;
-    struct sockaddr_in srv;
+    int status,
+        gniazdo,
+        dlugosc,
+        nr = 0,
+        end = 1,
+        gniazdo2;
+    struct sockaddr_in srv, cli;
     char buf[200];
 
     gniazdo = socket(AF_INET, SOCK_STREAM, 0);
@@ -17,22 +22,47 @@ int main(void)
 
     srv.sin_family = AF_INET;
     srv.sin_port = htons(9000);
-    srv.sin_addr.s_addr = inet_addr("127.0.0.1");
+    srv.sin_addr.s_addr = inet_addr("77.55.223.255");
 
-    printf("Podaj tekst: ");
-    fgets(buf, sizeof buf, stdin);
-
-    status = connect(gniazdo, (struct sockaddr *)&srv, sizeof srv);
-    if (status < 0)
+    status = bind(gniazdo, (struct sockaddr *)&srv, sizeof srv);
+    if (status == -1)
     {
-        printf("Connect error!\n");
+        printf("Bind error!\n");
         return 0;
     }
 
-    status = write(gniazdo, buf, strlen(buf));
-    status = read(gniazdo, buf, sizeof buf);
-    buf[status] = '\0';
-    printf("Otrzymalem: %s\n", buf);
+    status = listen(gniazdo, 10);
+    if (status == -1)
+    {
+        printf("Listen error\n");
+        return 0;
+    }
 
+    while (end)
+    {
+        dlugosc = sizeof cli;
+        gniazdo2 = accept(gniazdo, (struct sockaddr *)&cli, (socklen_t *)&dlugosc);
+        if (gniazdo2 == -1)
+        {
+            printf("Accept error!\n");
+            return 0;
+        }
+        read(gniazdo2, buf, sizeof buf);
+        if (buf[0] == 'Q')
+        {
+            sprintf(buf, "Terminating server...\n");
+            end = 0;
+        }
+        else if (buf[0] == 'N')
+        {
+            sprintf(buf, "Jestes klientem nr %d\0", nr++);
+        }
+        else
+        {
+            sprintf(buf, "Nie rozumiem pytania\0");
+        }
+        write(gniazdo2, buf, strlen(buf));
+        close(gniazdo2);
+    }
     close(gniazdo);
 }
