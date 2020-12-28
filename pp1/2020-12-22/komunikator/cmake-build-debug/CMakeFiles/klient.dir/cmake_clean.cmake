4b050d6be684f441593b38dc4b47654991f26e77
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2020-12-28 19:33:05 +0100

    Działający, ale zbugowany komunikator na Windows

---
+++ pp1/2020-12-22/komunikator/cmake-build-debug/CMakeFiles/klient.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/klient.dir/klient.c.obj"
+  "klient.exe"
+  "klient.exe.manifest"
+  "klient.lib"
+  "klient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/klient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pp1/2020-12-22/komunikator/cmake-build-debug/CMakeFiles/komunikator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/komunikator.dir/klient.c.obj"
+  "komunikator.exe"
+  "komunikator.exe.manifest"
+  "komunikator.lib"
+  "komunikator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/komunikator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
