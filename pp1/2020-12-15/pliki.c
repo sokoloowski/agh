e801c010fde6af41a3d6a283bdc952bcf44874a2
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2020-12-30 14:56:53 +0100

    Poprawione operacje na plikach

--- pp1/2020-12-15/pliki.c
+++ pp1/2020-12-15/pliki.c
@@ -3,16 +3,41 @@
 int main(void)
 {
     FILE *plik1, *plik2;
-    int w;
+    int w, val;
     plik1 = fopen("plik1.txt", "w");
     if (plik1 == 0)
+    {
         printf("Błąd podczas otwierania pliku\n");
+        return 0;
+    }
     for (int i = 0; i < 1000; i++)
     {
         w = fprintf(plik1, "%d ", i);
     }
     fclose(plik1);
 
+    // ------------ Przepisywanie ------------
+
     plik1 = fopen("plik1.txt", "r");
     plik2 = fopen("plik2.txt", "w");
+
+    if (plik1 == 0)
+    {
+        printf("Błąd podczas otwierania pliku plik1.txt\n");
+        return 0;
+    }
+    if (plik2 == 0)
+    {
+        printf("Błąd podczas otwierania pliku plik2.txt\n");
+        return 0;
+    }
+
+    for (int i = 0; i < 1000; i++)
+    {
+        fscanf(plik1, "%d", &val);
+        if (i % 2)
+        {
+            fprintf(plik2, "%d ", val);
+        }
+    }
 }
