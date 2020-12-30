51a1e16f871fca0ada2b5d5fa654e5d6fe0906af
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2020-12-30 16:00:58 +0100

    Pola bitowe

--- pp1/2020-12-15/polabitowe.c
+++ pp1/2020-12-15/polabitowe.c
@@ -1,4 +1,13 @@
-//
-// Created by Piotr Sokołowski on 30.12.2020.
-//
+#include <stdio.h>
 
+struct str1 {
+    unsigned int pole1: 1;
+    unsigned int byloPrzepelnienie: 1;
+    unsigned int pole3: 1;
+} flaga;
+
+int main(void) {
+    flaga.byloPrzepelnienie = 1;
+    flaga.pole3 = 0;
+    printf("%u %u\n", flaga.byloPrzepelnienie, flaga.pole3);
+}
